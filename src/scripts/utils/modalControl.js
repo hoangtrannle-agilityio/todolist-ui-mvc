2645320642c6cbe3ebfa1f3e87f7913806e25bac
Hoang Le Minh Tran <hoang.tranle@asnet.com.vn>
2024-02-21 10:05:39 +0700

    change background color for overlay modal

--- src/scripts/utils/modalControl.js
+++ src/scripts/utils/modalControl.js
@@ -2,7 +2,7 @@ import { getElement } from "./uiControl";
 
 export const openModal = (modalSelector) => {
   const modal = getElement(modalSelector);
-  modal.style.display = "block";
+  modal.style = "display: block; background-color: rgba(0,0,0,0.7);";
 };
 
 export const closeModal = (modalSelector) => {
